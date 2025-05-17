27d2d3f43f178fa7df9dc90d35cc142019c68989
Brighton <BrightonDube@users.noreply.github.com>
2025-05-17 23:16:08 +0200

    "Updated database schema and code to replace image paths and update Hummer description."

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -17,6 +17,14 @@ UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
+UPDATE public.inventory
+SET 
+    inv_image = REPLACE(inv_image, '/', '/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/', '/vehicles/')
+WHERE 
+    inv_image NOT LIKE '%/vehicles/%'
+    AND inv_thumbnail NOT LIKE '%/vehicles/%';
+
 SELECT 
     inv.inv_make, 
     inv.inv_model, 
@@ -25,3 +33,5 @@ FROM public.inventory inv
 INNER JOIN public.classification cls
     ON inv.classification_id = cls.classification_id
 WHERE cls.classification_name = 'Sport';
+
+
--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -240,3 +240,17 @@ VALUES   (
     'White',
     5
   );
+
+
+
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+UPDATE public.inventory
+SET 
+    inv_image = REPLACE(inv_image, '/', '/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/', '/vehicles/')
+WHERE 
+    inv_image NOT LIKE '%/vehicles/%'
+    AND inv_thumbnail NOT LIKE '%/vehicles/%';
