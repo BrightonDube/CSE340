eff1ac4cef68e128bd2d26ce2dc1c0244ec8cb0c
Brighton <BrightonDube@users.noreply.github.com>
2025-05-24 23:08:12 +0200

    Update server.js to use baseController for home route, improving modularity

--- server.js
+++ server.js
@@ -25,9 +25,8 @@ app.set('layout', './layouts/layout'); // not at views root
 app.use(static);
 
 // Index route
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Home' });
-});
+app.get('/', baseController.buildHome);
+
 
 /* ***********************
  * Local Server Information
