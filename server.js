b167fbd8c2ff0fa9c54d182a5c40297833e4283a
Brighton <BrightonDube@users.noreply.github.com>
2025-05-25 00:28:57 +0200

    Add custom animated error page with humorous 404/500 messages and dedicated CSS; improve error handling and routing

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const env = require('dotenv').config();
 const app = express();
 const static = require('./routes/static');
 const expressLayouts = require('express-ejs-layouts');
-const baseController = require("./controllers/baseController")
+const baseController = require("./controllers/baseController");
+const utilities = require("./utilities");
 
 /* ***********************
  * View Engine and Templates
@@ -26,7 +27,24 @@ app.use(static);
 
 // Index route
 app.get('/', baseController.buildHome);
-
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav,
+    status: err.status
+  })
+})
 
 /* ***********************
  * Local Server Information
